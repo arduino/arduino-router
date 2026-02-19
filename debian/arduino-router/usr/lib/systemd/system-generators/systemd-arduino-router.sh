f8f18730fa101124998881a05729fac90eb0f133
lucarin91 <lucarin@protonmail.com>
2026-02-19 16:09:37 +0100

    feat: add monza board support

--- debian/arduino-router/usr/lib/systemd/system-generators/systemd-arduino-router.sh
+++ debian/arduino-router/usr/lib/systemd/system-generators/systemd-arduino-router.sh
@@ -2,13 +2,20 @@
 
 TARGET_DIR="$1/arduino-router.service.d/"
 CONF_DIR="/var/lib/arduino-router/config"
-MODEL_FILE="/sys/class/dmi/id/product_name"
+MODEL_FILE1="/sys/class/dmi/id/product_name"
+MODEL_FILE2="/sys/firmware/devicetree/base/compatible"
 
-model=$(cat ${MODEL_FILE} | { read -r line; echo -n "$line"; })
+if [ -f "$MODEL_FILE1" ] ; then
+  model=$(cat $MODEL_FILE1 | { read -r line; echo -n "$line"; })
+elif [ -f "$MODEL_FILE2" ] ; then
+  model=$(cat $MODEL_FILE2 | tr '\0' ',' | cut -d',' -f2)
+fi
 
 mkdir -p "$TARGET_DIR"
 
 case "$model" in
-  "Imola"*)
+  "Imola"*|"imola"*)
     cp "${CONF_DIR}/10-imola.conf" "$TARGET_DIR" ;;
+  "Monza"*|"monza"*)
+    cp "${CONF_DIR}/10-monza.conf" "$TARGET_DIR" ;;
 esac
