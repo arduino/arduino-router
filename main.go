db236d2af3208de075c3730225967397ade43fdc
Giulio <pilotto.giulio@gmail.com>
2026-03-03 16:53:08 +0100

    Update debian/arduino-router/var/lib/arduino-router/config/10-imola.conf
    Co-authored-by: Luca Rinaldi <l.rinaldi@arduino.cc>

--- main.go
+++ main.go
@@ -46,13 +46,13 @@ var Version string = "0.0.0-dev"
 
 // Server configuration
 type Config struct {
-	LogLevel        slog.Level
-	ListenTCPAddr   string
-	ListenUnixAddr  string
+	LogLevel          slog.Level
+	ListenTCPAddr     string
+	ListenUnixAddr    string
 	AfterReadyCommand string
-	SerialPortAddr  string
-	SerialBaudRate  int
-	MonitorPortAddr string
+	SerialPortAddr    string
+	SerialBaudRate    int
+	MonitorPortAddr   string
 }
 
 func main() {
@@ -79,7 +79,7 @@ func main() {
 	cmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose logging")
 	cmd.Flags().StringVarP(&cfg.ListenTCPAddr, "listen-port", "l", "", "Listening port for RPC services")
 	cmd.Flags().StringVarP(&cfg.ListenUnixAddr, "unix-port", "u", "/var/run/arduino-router.sock", "Listening port for RPC services")
-	cmd.Flags().StringVar(&cfg.ExecCommand, "after-ready", "", "Execute a command after the router is ready)")
+	cmd.Flags().StringVar(&cfg.AfterReadyCommand, "after-ready", "", "Execute a command after the router is ready)")
 	cmd.Flags().StringVarP(&cfg.SerialPortAddr, "serial-port", "p", "", "Serial port address")
 	cmd.Flags().IntVarP(&cfg.SerialBaudRate, "serial-baudrate", "b", 115200, "Serial port baud rate")
 	cmd.Flags().StringVarP(&cfg.MonitorPortAddr, "monitor-port", "m", "127.0.0.1:7500", "Listening port for MCU monitor proxy")
@@ -288,21 +288,21 @@ func startRouter(cfg Config) error {
 		}()
 	}
 
-	if cfg.ExecCommand != "" {
+	if cfg.AfterReadyCommand != "" {
 		go func() {
 			// #nosec G204
-			p, err := paths.NewProcess(nil, "sh", "-c", cfg.ExecCommand)
+			p, err := paths.NewProcess(nil, "sh", "-c", cfg.AfterReadyCommand)
 			if err != nil {
-				slog.Error("Failed to create process for exec command", "command", cfg.ExecCommand, "err", err)
+				slog.Error("Failed to create process for after-ready command", "command", cfg.AfterReadyCommand, "err", err)
 				return
 			}
 			p.RedirectStderrTo(io.Discard)
 			p.RedirectStdoutTo(io.Discard)
 			if err := p.Run(); err != nil {
-				slog.Error("Failed to start exec command", "command", cfg.ExecCommand, "err", err)
+				slog.Error("Failed to start after-ready command", "command", cfg.AfterReadyCommand, "err", err)
 				return
 			}
-			slog.Info("Executed command successfully", "command", cfg.ExecCommand)
+			slog.Info("Executed after-ready command successfully", "command", cfg.AfterReadyCommand)
 		}()
 	}
 
