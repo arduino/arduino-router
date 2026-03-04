7175d37412ff308559f226ffaee2ed5a22828994
Cristian Maglie <c.maglie@arduino.cc>
2026-03-04 15:55:46 +0100

    Adopt gpl-3.0-or-later

--- internal/hciapi/hci-api_default.go
+++ internal/hciapi/hci-api_default.go
@@ -2,10 +2,18 @@
 //
 // Copyright (C) Arduino s.r.l. and/or its affiliated companies
 //
-// This software is released under the GNU General Public License version 3,
-// which covers the main part of arduino-app-cli.
-// The terms of this license can be found at:
-// https://www.gnu.org/licenses/gpl-3.0.en.html
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 // You can be released from the requirements of the above licenses by purchasing
 // a commercial license. Buying such a license is mandatory if you want to
--- internal/msgpackrouter/errors.go
+++ internal/msgpackrouter/errors.go
@@ -1,11 +1,19 @@
-// This file is part of arduino-router
+// This file is part of arduino-router.
 //
-// Copyright (C) ARDUINO SRL (www.arduino.cc)
+// Copyright (C) Arduino s.r.l. and/or its affiliated companies
 //
-// This software is released under the GNU General Public License version 3,
-// which covers the main part of arduino-router
-// The terms of this license can be found at:
-// https://www.gnu.org/licenses/gpl-3.0.en.html
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 // You can be released from the requirements of the above licenses by purchasing
 // a commercial license. Buying such a license is mandatory if you want to
